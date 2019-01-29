7d4145f1145b17221681026e0174216fdcb157bd
Piush Singh <43833@hexaware.com>
2019-01-29 12:32:55 +0530

    s

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 const restService = express();
+require('deasync'); 
 
 restService.use(
   bodyParser.urlencoded({
@@ -17,7 +18,6 @@ var server= require('http').createServer(restService)
 var io= require('socket.io')(server);
 
 var responseObj;
-
 restService.post("/api",function(req,res){
 console.log("received a post request");
 if(!req.body) return res.sendStatus(400);
@@ -51,49 +51,12 @@ console.log("response data " + JSON.stringify(responseObj));
 return res.json(responseObj);
 
 });
-
-
-
-// restService.post("/cal", function(req, res) {
-  
-//   if (req.body.result.parameters.addition == "add"){
-//     result =
-//     req.body.result &&
-//     req.body.result.parameters &&
-//     req.body.result.parameters.first_number
-//       ? parseInt(req.body.result.parameters.second_number )+ parseInt(req.body.result.parameters.first_number)
-//       : "Seems like some problem from addition. Speak again.";
-//   }
-
-//   else if(req.body.result.parameters.subtraction == "sub")
-//   {
-//     result =
-//     req.body.result &&
-//     req.body.result.parameters &&
-//     req.body.result.parameters.first_number
-//       ? req.body.result.parameters.second_number - req.body.result.parameters.first_number
-//       : "Seems like some problem for subtraction. Speak again.";
-//   }
-
-
-//   return res.json({
-//     speech: result,
-//     displayText: result,
-//     source: "calc-app"
-//   });
-// });
-
-
-
-// apiKey=a460fad55c48ec9d205741d13b1376fb
-
  var result ;
-
  function cb(err,response,body){
-     //  var weather = '';
-  //    var stringify = JSON.stringify(body)
-  //    weather = JSON.parse(stringify);
-     var weather = JSON.parse(body);
+    
+  //var parse = JSON.parse(body)
+  //    var weather = JSON.stringify(parse);
+    var weather = JSON.parse(body);
      console.log("weather data " + weather);
      if(weather.messasge === 'city not found')
      {
@@ -116,14 +79,6 @@ return res.json(responseObj);
         return result;
     }
 
-
-
-
-
-
-
-
-
-restService.listen(process.env.PORT || 5000, function() {
+restService.listen(process.env.PORT || 5050, function() {
   console.log("Server up and listening");
 });
